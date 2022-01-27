a0f6d8384141f24cc54a4df53a9819c64234224b
cavani12345 <edouardkwizera2000@gmail.com>
2022-01-26 22:05:12 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","You, as young person, remember the problem is not time. It is you to start.","","Let's make a very productive community in our ability","Be yourself and do the best you can in your ability"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
